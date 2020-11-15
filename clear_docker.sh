ebc72b858a6bb163579cb714b1e6f851b095b5b3
andybe <3150627+andybe@users.noreply.github.com>
2020-11-15 20:14:07 +0100

    revert network

--- clear_docker.sh
+++ clear_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker ps -a | awk '{ print $1 }' | grep -v CONTAINER | xargs docker rm -f
 docker volume ls |  awk '{ print $2 }' | grep -v VOLUME | xargs docker volume rm
-docker network ls | awk '{ print $1 }' | grep -v NETWORK | xargs docker network rm -f
+docker network ls | awk '{ print $1 }' | grep -v NETWORK | xargs docker network rm
 docker images |  awk '{ print $3 }' | grep -v IMAGE | xargs docker rmi
