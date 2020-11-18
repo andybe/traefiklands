d93bd9207455793b8891e9d641dbdcf7563b7708
andybe <3150627+andybe@users.noreply.github.com>
2020-11-18 07:33:12 +0100

    typo

--- 06_private_ssl/create_certificate.sh
+++ 06_private_ssl/create_certificate.sh
@@ -21,3 +21,20 @@ if [ ! -f "$CERT" ]; then
    chmod 600 $KEY
 
 fi
+
+openssl genrsa -out rootCA.key 4096
+
+
+openssl genrsa -out traefik.key 2048
+
+openssl req -new -sha256 \
+    -key traefik.key \     
+    -subj "/C=DE/ST=B/O=Traefiklands Rocks/CN=traefiklands.rocks" \
+    -reqexts SAN \
+    -config <(cat /etc/ssl/openssl.cnf \
+        <(printf "\n[SAN]\nsubjectAltName=DNS:traefiklands.rocks,DNS:*.traefiklands.rocks")) \
+    -out traefik.csr 
+
+openssl x509 -req -in traefik.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out traefik.crt -days 3650 -sha256
+
+openssl x509 -in mydomain.com.crt -text -noout
